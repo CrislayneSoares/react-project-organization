bbadb914d664e227c7f88bc431ce7f504408f4c6
Crislayne <crissoaresr@hotmail.com>
2023-11-17 21:37:34 -0300

    create component lado esquerdo

---
+++ src/components/componenteLEsquerdo/ComponenteLEsquerdo.jsx
@@ -0,0 +1,10 @@
+import Search from '../search/Search'
+import './Style.sass'
+
+export default function ComponenteLEsquerdo() {
+    return (
+        <section className='container__esquerdo'>
+            <Search/>
+        </section>
+    )
+}
